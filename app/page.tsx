39027fd71ac72baa597b44eab5547987adb19f4e
dispensador <135203479+cordero1794@users.noreply.github.com>
2026-05-20 23:56:08 -0500

    Adjust environment scale

--- app/page.tsx
+++ app/page.tsx
@@ -243,8 +243,8 @@ export default function Home() {
             </div>
           </div>
 
-           <div className="grid lg:grid-cols-[5fr_5fr] gap-4 items-start max-w-5xl ml-0">
-            <div className="bg-zinc-950/90 border border-zinc-800 rounded-[2.2rem] p-2.5 -mt-305">
+           <div className="grid lg:grid-cols-[9fr_9fr] gap-4 items-start max-w-5xl ml-0">
+            <div className="bg-zinc-950/90 border border-zinc-800 rounded-[3.2rem] p-2.5 -mt-305">
               <h3 className="text-xl font-black mb-1">
                 Configuración experimental
               </h3>
